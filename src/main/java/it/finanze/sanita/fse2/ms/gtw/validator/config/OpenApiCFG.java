a15d189db7283d20c48349329d869922537d98fe
pierascenzi <pierascenzi@gmail.com>
2022-05-02 23:03:20 +0200

    Create OpenApiCFG.java

---
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/config/OpenApiCFG.java
@@ -0,0 +1,16 @@
+package it.finanze.sanita.fse2.ms.gtw.validator.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Gateway Validator", version = "v1"))
+public class OpenApiCFG {
+
+
+  public OpenApiCFG() {
+  }
+
+}
