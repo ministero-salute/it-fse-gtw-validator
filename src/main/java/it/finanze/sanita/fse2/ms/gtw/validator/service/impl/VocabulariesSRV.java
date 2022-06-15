278480c66c93250b68160339d02e21f6b56af62b
Vincenzo Ingenito <Vincenzo.Ingenito-CIC@ibm.com>
2022-06-15 13:01:59 +0200

    feat(Sprint2b) : Uat_200622

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/service/impl/VocabulariesSRV.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/service/impl/VocabulariesSRV.java
@@ -66,7 +66,7 @@ public class VocabulariesSRV implements IVocabulariesSRV {
             				List<String> differences = terminologyList.stream().filter(element -> !findedCodes.contains(element)).collect(Collectors.toList());
             				if(!differences.isEmpty()) {
             					log.info("Not all codes for system {} are present on Mongo", system);
-            					vocaboliInesistenti = "[Dizionario : " + system + " ,Vocaboli:" + differences.stream().collect(Collectors.joining(",")) + "]";
+            					vocaboliInesistenti = differences.stream().collect(Collectors.joining(","));
             					exists = false;
             					break;
             				}
