5781d9a15318604922c3a8c413eea6f230ffe17c
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2022-06-20 12:48:55 +0200

    Remove custom eq-hash implementation to favor lombok one

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/repository/entity/TerminologyETY.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/repository/entity/TerminologyETY.java
@@ -1,17 +1,18 @@
 package it.finanze.sanita.fse2.ms.gtw.validator.repository.entity;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * Model of terminology.
  */
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode
 @Document(collection = "terminology")
 public class TerminologyETY {
 
@@ -26,36 +27,4 @@ public class TerminologyETY {
 	
 	@Field(name = "description")
 	private String description;
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		TerminologyETY other = (TerminologyETY) obj;
-		if (code == null) {
-			if (other.code != null)
-				return false;
-		} else if (!code.equals(other.code))
-			return false;
-		if (system == null) {
-			if (other.system != null)
-				return false;
-		} else if (!system.equals(other.system))
-			return false;
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((code == null) ? 0 : code.hashCode());
-		result = prime * result + ((system == null) ? 0 : system.hashCode());
-		return result;
-	}
-	 
 }
