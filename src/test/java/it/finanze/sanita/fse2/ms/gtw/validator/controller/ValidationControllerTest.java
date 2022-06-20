3a26917dedbf2bed45d2ff5e69fc41ef598065ca
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2022-06-20 15:20:09 +0200

    fix(Tests): Add more tests

---
+++ src/test/java/it/finanze/sanita/fse2/ms/gtw/validator/controller/ValidationControllerTest.java
@@ -0,0 +1,44 @@
+package it.finanze.sanita.fse2.ms.gtw.validator.controller;
+
+import it.finanze.sanita.fse2.ms.gtw.validator.config.Constants;
+import it.finanze.sanita.fse2.ms.gtw.validator.dto.request.ValidationRequestDTO;
+import it.finanze.sanita.fse2.ms.gtw.validator.dto.response.ValidationResponseDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static it.finanze.sanita.fse2.ms.gtw.validator.utility.FileUtility.getFileFromInternalResources;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ComponentScan(basePackages = { Constants.ComponentScan.BASE })
+@ActiveProfiles(Constants.Profile.TEST)
+public class ValidationControllerTest {
+
+    public static final Path TEST_FILE = Paths.get(
+        "Files",
+        "cda1.xml"
+    );
+
+    @Autowired
+    private IValidationCTL controller;
+
+    @Test
+    void validate() {
+        ValidationResponseDTO res = controller.validation(new ValidationRequestDTO(
+            new String(
+                getFileFromInternalResources(TEST_FILE.toString()),
+                StandardCharsets.UTF_8
+            )
+        ), new MockHttpServletRequest());
+        assertNotNull(res);
+    }
+
+}
--- src/test/java/it/finanze/sanita/fse2/ms/gtw/validator/service/UpdateSingletonSRVTest.java
+++ src/test/java/it/finanze/sanita/fse2/ms/gtw/validator/service/UpdateSingletonSRVTest.java
@@ -1,18 +1,17 @@
 package it.finanze.sanita.fse2.ms.gtw.validator.service;
 
 import it.finanze.sanita.fse2.ms.gtw.validator.config.Constants;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles(Constants.Profile.TEST)
 @ComponentScan(basePackages = { Constants.ComponentScan.BASE })
+@ActiveProfiles(Constants.Profile.TEST)
 public class UpdateSingletonSRVTest {
 
     @Autowired
