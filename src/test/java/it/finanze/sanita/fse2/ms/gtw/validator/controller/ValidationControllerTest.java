2a44626f2a60ca810917618537e270ac5485a83e
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2022-06-21 10:55:57 +0200

    fix(Tests): Add another test path

--- src/test/java/it/finanze/sanita/fse2/ms/gtw/validator/controller/ValidationControllerTest.java
+++ src/test/java/it/finanze/sanita/fse2/ms/gtw/validator/controller/ValidationControllerTest.java
@@ -3,6 +3,7 @@ package it.finanze.sanita.fse2.ms.gtw.validator.controller;
 import it.finanze.sanita.fse2.ms.gtw.validator.config.Constants;
 import it.finanze.sanita.fse2.ms.gtw.validator.dto.request.ValidationRequestDTO;
 import it.finanze.sanita.fse2.ms.gtw.validator.dto.response.ValidationResponseDTO;
+import it.finanze.sanita.fse2.ms.gtw.validator.enums.RawValidationEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +16,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static it.finanze.sanita.fse2.ms.gtw.validator.utility.FileUtility.getFileFromInternalResources;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ComponentScan(basePackages = { Constants.ComponentScan.BASE })
@@ -25,6 +26,12 @@ public class ValidationControllerTest {
     public static final Path TEST_FILE = Paths.get(
         "Files",
         "cda1.xml"
+    );
+
+    public static final Path TEST_FILE_ERR = Paths.get(
+        "Files",
+        "cda_ok",
+        "Esempio CDA_002.xml"
     );
 
     @Autowired
@@ -38,7 +45,17 @@ public class ValidationControllerTest {
                 StandardCharsets.UTF_8
             )
         ), new MockHttpServletRequest());
-        assertNotNull(res);
+        assertEquals(res.getResult().getResult(), RawValidationEnum.SYNTAX_ERROR);
+
+        res = controller.validation(new ValidationRequestDTO(
+            new String(
+                getFileFromInternalResources(TEST_FILE_ERR.toString()),
+                StandardCharsets.UTF_8
+            )
+        ), new MockHttpServletRequest());
+
+        assertEquals(res.getResult().getResult(), RawValidationEnum.VOCABULARY_ERROR);
+
     }
 
 }
