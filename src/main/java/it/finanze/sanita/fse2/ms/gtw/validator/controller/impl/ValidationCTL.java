3a9a80eaf6a2d444f4d2e5af4009fecbbada096e
Vincenzo Ingenito <Vincenzo.Ingenito-CIC@ibm.com>
2022-06-24 13:06:37 +0200

    feat(Merge) : allineamento uat

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/controller/impl/ValidationCTL.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/controller/impl/ValidationCTL.java
@@ -72,13 +72,16 @@ public class ValidationCTL extends AbstractCTL implements IValidationCTL {
 
 		if(RawValidationEnum.OK.equals(outcome)) {
 			SchematronValidationResultDTO semanticValidation = validationSRV.validateSemantic(requestBody.getCda(),infoDTO);
-			if(!semanticValidation.getValidXML()) {
+			if(semanticValidation.getFailedAssertions()!= null && !semanticValidation.getFailedAssertions().isEmpty()) {
 				for(SchematronFailedAssertionDTO violation : semanticValidation.getFailedAssertions()) {
 					messages.add(violation.getText());
-
 				}
-				outcome = RawValidationEnum.SEMANTIC_ERROR;
+				outcome = RawValidationEnum.SEMANTIC_WARNING;
+				if(Boolean.FALSE.equals(semanticValidation.getValidXML())){
+					outcome = RawValidationEnum.SEMANTIC_ERROR;
+				}
 			}
+
 
 			if(RawValidationEnum.OK.equals(outcome)) {
 				VocabularyResultDTO result =  validationSRV.validateVocabularies(requestBody.getCda());
