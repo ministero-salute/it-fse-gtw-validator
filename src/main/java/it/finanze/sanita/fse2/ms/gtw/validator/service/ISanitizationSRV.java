a58ef5028cd6048d63cf0bd00b339e1359b6feca
Andrea Prosseda <andreaprosseda@gmail.com>
2022-11-22 09:08:06 +0100

    feat(refactor): unused service removed

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/service/ISanitizationSRV.java
+++
@@ -1,9 +0,0 @@
-package it.finanze.sanita.fse2.ms.gtw.validator.service;
-
-import java.io.Serializable;
-
-public interface ISanitizationSRV extends Serializable {
-	
-	String sanitizeCda(String cda);
-	
-}
