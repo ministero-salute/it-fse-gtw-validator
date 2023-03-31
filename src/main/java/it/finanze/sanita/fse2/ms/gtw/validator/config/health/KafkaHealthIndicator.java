a73ec350e1ca0f92d2ff6d540795903f01da82e4
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2023-03-31 17:32:16 +0200

    Revert "Update KafkaHealthIndicator.java"
    This reverts commit a94c26666d1fc46916b9f035272872c0850bda91.

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/config/health/KafkaHealthIndicator.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/config/health/KafkaHealthIndicator.java
@@ -1,34 +1,34 @@
-//package it.finanze.sanita.fse2.ms.gtw.validator.config.health;
-//
-//import org.apache.kafka.clients.admin.AdminClient;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.actuate.health.Health;
-//import org.springframework.boot.actuate.health.HealthIndicator;
-//import org.springframework.stereotype.Component;
-//
-//import lombok.extern.slf4j.Slf4j;
-//
-//@Component
-//@Slf4j
-//public class KafkaHealthIndicator implements HealthIndicator {
-//
-//    @Autowired
-//    private AdminClient client;
-//	
-//    @Override
-//   	public Health health() {
-//   		Health health = null;
-//   		try {
-//   			client.listTopics().listings().get();
-//   			health = Health.up().build();
-//   		} catch (InterruptedException e) {
-//   			log.warn("Interrupted!", e);
-//   			health = Health.down(e).build();
-//   			// Restore interrupted state...
-//   			Thread.currentThread().interrupt();
-//   		} catch (Exception e) {
-//   			health = Health.down(e).build();
-//   		}
-//   		return health;
-//   	}
-//}
+package it.finanze.sanita.fse2.ms.gtw.validator.config.health;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class KafkaHealthIndicator implements HealthIndicator {
+
+    @Autowired
+    private AdminClient client;
+	
+    @Override
+   	public Health health() {
+   		Health health = null;
+   		try {
+   			client.listTopics().listings().get();
+   			health = Health.up().build();
+   		} catch (InterruptedException e) {
+   			log.warn("Interrupted!", e);
+   			health = Health.down(e).build();
+   			// Restore interrupted state...
+   			Thread.currentThread().interrupt();
+   		} catch (Exception e) {
+   			health = Health.down(e).build();
+   		}
+   		return health;
+   	}
+}
