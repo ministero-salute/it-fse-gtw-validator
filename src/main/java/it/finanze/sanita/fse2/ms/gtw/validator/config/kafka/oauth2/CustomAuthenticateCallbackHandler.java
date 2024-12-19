3f69dcb0abe3c7fbc6e4d61f2f6e6d01a43db6f3
Andrea De Lella <Andrea.De.Lella-CIC@ibm.com>
2024-12-19 17:08:48 +0100

    fix: sonarqube issue

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/config/kafka/oauth2/CustomAuthenticateCallbackHandler.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/config/kafka/oauth2/CustomAuthenticateCallbackHandler.java
@@ -101,7 +101,6 @@ public class CustomAuthenticateCallbackHandler implements AuthenticateCallbackHa
     private OAuthBearerToken getOAuthBearerToken() throws MalformedURLException, InterruptedException, ExecutionException, TimeoutException {
         if (this.aadClient == null) {
             synchronized(this) {
-                if (this.aadClient == null) {
                 	IClientCredential credential = null;
             	    try (FileInputStream certificato = new FileInputStream(new File(pfxPathName))) {
                 		credential = ClientCredentialFactory.createFromCertificate(certificato, this.pwd);	
@@ -112,7 +111,6 @@ public class CustomAuthenticateCallbackHandler implements AuthenticateCallbackHa
                     this.aadClient = ConfidentialClientApplication.builder(this.appId, credential)
                             .authority(this.tenantId)
                             .build();
-                }
             }
         }
 
