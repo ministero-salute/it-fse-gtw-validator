34b422f68505f291ad565aec950da4be04df8fc4
Simone Lungarella <simonelungarella@gmail.com>
2025-04-10 18:40:31 +0200

    fix: corrected VPS schematron test to avoid ignore files

--- src/test/java/it/finanze/sanita/fse2/ms/gtw/validator/VPSSchematronTest.java
+++ src/test/java/it/finanze/sanita/fse2/ms/gtw/validator/VPSSchematronTest.java
@@ -12,7 +12,9 @@
 package it.finanze.sanita.fse2.ms.gtw.validator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayInputStream;
@@ -25,6 +27,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.util.CollectionUtils;
 
 import com.helger.commons.io.resource.IReadableResource;
 import com.helger.commons.io.resource.inmemory.ReadableResourceInputStream;
@@ -39,57 +42,60 @@ import it.finanze.sanita.fse2.ms.gtw.validator.service.impl.ConfigSRV;
 import it.finanze.sanita.fse2.ms.gtw.validator.utility.FileUtility;
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(Constants.Profile.TEST)
 class VPSSchematronTest extends AbstractTest {
 
-	@MockitoBean
-	private ConfigSRV config;
-	 
-	@Test
-	@DisplayName("CDA OK")
-	void cdaOK() throws Exception {
-		when(config.isAuditEnable()).thenReturn(true);
+    @MockitoBean
+    private ConfigSRV config;
 
-		byte[] schematron = FileUtility.getFileFromInternalResources("Files" + File.separator + "schematronVPS" + File.separator + "schV3" + File.separator +"schematron_VPS_v 2.7.sch");
+    @Test
+    @DisplayName("CDA OK")
+    void cdaOK() throws Exception {
+        when(config.isAuditEnable()).thenReturn(true);
+        final String folder = "Files" + File.separator + "schematronVPS" + File.separator + "schV3" + File.separator;
+        final String filename = "schematron_VPS_v 2.7.sch";
 
-		try (ByteArrayInputStream bytes = new ByteArrayInputStream(schematron)) {
-			IReadableResource readableResource = new ReadableResourceInputStream("schematron_VPS_v 2.7.sch", bytes);
-			SchematronResourceSCH schematronResource = new SchematronResourceSCH(readableResource);
-			Map<String,byte[]> cdasOK = getSchematronFiles(SchematronPath.VPS.OK());
-			for(Entry<String, byte[]> cdaOK : cdasOK.entrySet()) {
-				log.info("File analyzed :" + cdaOK.getKey());
-				SchematronValidationResultDTO resultDTO = CDAHelper.validateXMLViaSchematronFull(schematronResource, cdaOK.getValue());
-				assertEquals(0, resultDTO.getFailedAssertions().size());
-				assertEquals(true, resultDTO.getValidSchematron());
-				assertEquals(true, resultDTO.getValidXML());
-			}
-		}
-		
-	}
-	
-	@Test
-	@DisplayName("CDA ERROR")
-	void cdaError() throws Exception {
-		when(config.isAuditEnable()).thenReturn(true);
+        byte[] content = FileUtility.getFileFromInternalResources(folder + filename);
 
-		byte[] schematron = FileUtility.getFileFromInternalResources("Files" + File.separator + "schematronVPS" + File.separator + "schV3" + File.separator +"schematron_VPS_v 2.7.sch");
-		
-		try (ByteArrayInputStream bytes = new ByteArrayInputStream(schematron)) {
-			IReadableResource readableResource = new ReadableResourceInputStream("schematron_VPS_v 2.7.sch", bytes);
-			SchematronResourceSCH schematronResource = new SchematronResourceSCH(readableResource);
-			
-			Map<String,byte[]> cdasKO = getSchematronFiles(SchematronPath.VPS.ERROR());
-			for(Entry<String, byte[]> cdaKO : cdasKO.entrySet()) {
-				
-				SchematronValidationResultDTO resultDTO = CDAHelper.validateXMLViaSchematronFull(schematronResource, cdaKO.getValue());
-				boolean result = resultDTO.getFailedAssertions().size()>0;
-				assertTrue(result);
-			}
-		}
-	}
-   
-	 
+        try (ByteArrayInputStream bytes = new ByteArrayInputStream(content)) {
+            IReadableResource resource = new ReadableResourceInputStream(filename, bytes);
+            SchematronResourceSCH sch = new SchematronResourceSCH(resource);
+            Map<String, byte[]> cdas = getSchematronFiles(SchematronPath.VPS.OK());
+
+            assumeFalse(CollectionUtils.isEmpty(cdas.values()));
+            for (Entry<String, byte[]> cda : cdas.entrySet()) {
+
+                SchematronValidationResultDTO result = CDAHelper.validateXMLViaSchematronFull(sch, cda.getValue());
+                assertEquals(0, result.getFailedAssertions().size());
+                assertTrue(result.getValidSchematron());
+                assertTrue(result.getValidXML());
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("CDA ERROR")
+    void cdaError() throws Exception {
+        when(config.isAuditEnable()).thenReturn(true);
+        final String folder = "Files" + File.separator + "schematronVPS" + File.separator + "schV3" + File.separator;
+        final String filename = "schematron_VPS_v 2.7.sch";
+
+        byte[] content = FileUtility.getFileFromInternalResources(folder + filename);
+
+        try (ByteArrayInputStream bytes = new ByteArrayInputStream(content)) {
+            IReadableResource resource = new ReadableResourceInputStream("schematron_VPS_v 2.7.sch", bytes);
+            SchematronResourceSCH sch = new SchematronResourceSCH(resource);
+
+            Map<String, byte[]> cdas = getSchematronFiles(SchematronPath.VPS.KO());
+
+            assumeFalse(CollectionUtils.isEmpty(cdas.values()));
+            for (Entry<String, byte[]> cda : cdas.entrySet()) {
+                SchematronValidationResultDTO result = CDAHelper.validateXMLViaSchematronFull(sch, cda.getValue());
+                assertFalse(result.getValidXML());
+            }
+        }
+    }
+
 }
