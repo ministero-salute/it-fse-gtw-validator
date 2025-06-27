49ca5ed7685a50126db40d39be38c19095813152
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2025-06-27 17:19:49 +0200

    feat: Update pom and props

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/config/OpenTelemetryConfig.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/validator/config/OpenTelemetryConfig.java
@@ -1,16 +1,16 @@
-//package it.finanze.sanita.fse2.ms.gtw.validator.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import io.opentelemetry.api.GlobalOpenTelemetry;
-//import io.opentelemetry.api.trace.Tracer;
-//import static it.finanze.sanita.fse2.ms.gtw.validator.config.Constants.Properties.MS_NAME;
-//
-//@Configuration
-//public class OpenTelemetryConfig {
-//
-//    @Bean
-//    public Tracer tracer() {
-//        return GlobalOpenTelemetry.getTracer(MS_NAME);
-//    }
-//}
+package it.finanze.sanita.fse2.ms.gtw.validator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
+import static it.finanze.sanita.fse2.ms.gtw.validator.config.Constants.Properties.MS_NAME;
+
+@Configuration
+public class OpenTelemetryConfig {
+
+    @Bean
+    public Tracer tracer() {
+        return GlobalOpenTelemetry.getTracer(MS_NAME);
+    }
+}
